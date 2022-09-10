5bd75cab424349d718dfded291a537facbfc4d16
Jose Faro <blakmetall@gmail.com>
2022-09-09 19:22:50 -0500

    feat: pagina de home beta, elementos de diseño agregados, eventos básicos agregados, datos dummy para favoritos

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,143 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button, FavoritesBar, MemberCard } from '../../components';
+import { loadStaff } from '../../store/actions/staff';
+import { loadStudents } from '../../store/actions/students';
+import {
+    StyledButtonsWrapper,
+    StyledContainer,
+    StyledFooterContainer,
+    StyledHeadingContainer,
+    StyledHeadingWrapper,
+    StyledLogo,
+    StyledLogoWrapper,
+    StyledMembersWrapper,
+    StyledSelectFilterLabel,
+} from './styled';
+
+// favorites dummy
+const favorites = [
+    {
+        id: 1,
+        name: 'Luna Lovegood',
+        image: 'http://hp-api.herokuapp.com/images/mcgonagall.jpg',
+    },
+    {
+        id: 2,
+        name: 'Luna Lovegood',
+        image: 'http://hp-api.herokuapp.com/images/mcgonagall.jpg',
+    },
+    {
+        id: 3,
+        name: 'Luna Lovegood',
+        image: 'http://hp-api.herokuapp.com/images/mcgonagall.jpg',
+    },
+    {
+        id: 4,
+        name: 'Luna Lovegood',
+        image: 'http://hp-api.herokuapp.com/images/mcgonagall.jpg',
+    },
+    {
+        id: 5,
+        name: 'Luna Lovegood',
+        image: 'http://hp-api.herokuapp.com/images/mcgonagall.jpg',
+    },
+];
 
 const HomePage = () => {
-    return <div>Home page</div>;
+    const dispatch = useDispatch();
+
+    const members = useSelector((state) => state.members.members);
+
+    console.log(members);
+
+    useEffect(() => {
+        // load students on load
+        dispatch(loadStudents());
+    }, []);
+
+    const handleOnAddFavorites = () => {
+        console.log('add favorites');
+    };
+
+    const handleOnDeleteFavorite = (favoriteId) => {
+        console.log('delete favorite ' + favoriteId);
+    };
+
+    const handleOnShowStudents = () => {
+        // load students
+        dispatch(loadStudents());
+    };
+
+    const handleOnShowStaff = (favoriteId) => {
+        // load staff
+        dispatch(loadStaff());
+    };
+
+    return (
+        <>
+            <StyledHeadingContainer>
+                <StyledContainer>
+                    {/* heading */}
+                    <StyledHeadingWrapper>
+                        <div className="d-flex justify-content-end">
+                            <FavoritesBar
+                                favorites={favorites}
+                                onAddFavorites={handleOnAddFavorites}
+                                onDeleteFavorite={handleOnDeleteFavorite}
+                            />
+                        </div>
+                    </StyledHeadingWrapper>
+
+                    {/* logo */}
+                    <StyledLogoWrapper>
+                        <div className="d-flex justify-content-center">
+                            <StyledLogo src="images/logo.svg" alt="" />
+                        </div>
+                    </StyledLogoWrapper>
+
+                    {/* filter label */}
+                    <StyledSelectFilterLabel>Selecciona tu filtro</StyledSelectFilterLabel>
+
+                    {/* buttons */}
+                    <StyledButtonsWrapper>
+                        <div>
+                            <Button label={'Estudiantes'} onClick={handleOnShowStudents} />
+                        </div>
+
+                        <div className="pe-1" />
+
+                        <div>
+                            <Button label={'Staff'} onClick={handleOnShowStaff} />
+                        </div>
+                    </StyledButtonsWrapper>
+                </StyledContainer>
+            </StyledHeadingContainer>
+
+            {/* members list */}
+            {members.length && (
+                <StyledContainer>
+                    <StyledMembersWrapper>
+                        {members.map((member, index) => (
+                            <MemberCard key={index} member={member} />
+                        ))}
+                    </StyledMembersWrapper>
+                </StyledContainer>
+            )}
+
+            <StyledFooterContainer>
+                <StyledContainer>
+                    <div className="d-flex justify-content-center">
+                        <FavoritesBar
+                            favorites={favorites}
+                            onAddFavorites={handleOnAddFavorites}
+                            onDeleteFavorite={handleOnDeleteFavorite}
+                        />
+                    </div>
+                </StyledContainer>
+            </StyledFooterContainer>
+        </>
+    );
 };
 
 export default HomePage;
---
+++ src/pages/home/styled.js
@@ -0,0 +1,111 @@
+import styled from 'styled-components';
+
+const StyledButtonsWrapper = styled.div`
+    max-width: 670px;
+    margin: 0 auto 80px auto;
+    display: flex;
+    justify-content: space-between;
+
+    @media (max-width: 767px) {
+        justify-content: space-evenly;
+    }
+`;
+
+const StyledContainer = styled.div`
+    width: 100%;
+    max-width: 1120px;
+    margin: 0 auto;
+    padding: 0 20px;
+`;
+
+const StyledFooterContainer = styled.div`
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    display: none;
+
+    @media (max-width: 767px) {
+        display: block;
+    }
+`;
+
+const StyledHeadingContainer = styled.div`
+    background: url('/images/background.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: bottom center;
+    min-height: 650px;
+
+    @media (max-width: 991px) {
+        min-height: 500px;
+    }
+
+    @media (max-width: 767px) {
+        background: none;
+    }
+`;
+
+const StyledHeadingWrapper = styled.div`
+    margin-bottom: 150px;
+
+    @media (max-width: 991px) {
+        margin-bottom: 80px;
+    }
+
+    @media (max-width: 767px) {
+        display: none;
+    }
+`;
+
+const StyledLogo = styled.img`
+    @media (max-width: 767px) {
+        width: 124px;
+    }
+`;
+
+const StyledLogoWrapper = styled.div`
+    margin-bottom: 30px;
+
+    @media (max-width: 767px) {
+        padding-top: 30px;
+    }
+`;
+
+const StyledMembersWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-top: -150px;
+
+    @media (max-width: 991px) {
+        margin-top: -90px;
+    }
+
+    @media (max-width: 767px) {
+        margin-top: -260px;
+    }
+`;
+
+const StyledSelectFilterLabel = styled.div`
+    font-size: 36px;
+    text-align: center;
+    font-weight: 600;
+    margin-bottom: 50px;
+
+    @media (max-width: 767px) {
+        font-size: 18px;
+        margin-bottom: 30px;
+    }
+`;
+
+export {
+    StyledButtonsWrapper,
+    StyledContainer,
+    StyledFooterContainer,
+    StyledHeadingContainer,
+    StyledHeadingWrapper,
+    StyledLogo,
+    StyledLogoWrapper,
+    StyledMembersWrapper,
+    StyledSelectFilterLabel,
+};
